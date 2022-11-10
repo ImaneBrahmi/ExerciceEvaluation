6ed5267ea3b7783d70b01d740296c41c4c8d7ec3
Hp <Hp@DESKTOP-C75HHEU>
2022-11-10 23:01:09 +0100

    Add project files.

---
+++ ExerciceEvaluation/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciceEvaluation;
+
+class Program
+{
+    class Etudiant
+    {
+        private char nom;
+        private char prenom;
+        private int Numeroetudiant;
+    }
+    class Evaluation
+    {
+        private int datedebut;
+        private int datefin;
+    }
+    class Question
+    {
+        private float nbr;
+    }
+    class Qsm : Question
+    {
+        private int nbquestion;
+    }
+    class QuestionOvert : Question
+    {
+        private int NBQUESTION;
+    }
+    class Alternation
+    {
+        private string choix1;
+        private string choix2;
+        private string choix3;
+    }
+    class SeulChoix : Alternation
+    {
+
+    }
+    class MultiChoix : Alternation
+    {
+
+    }
+    class reponse
+    {
+        private char choix;
+        public char Verifreponse()
+        {
+            return choix;
+        }
+    }
+
+    class degredeCertitude
+    {
+        private int degre;
+
+        public int verifiDegre()
+        {
+            return degre;
+        }
+    }
+    class Passur
+    {
+
+    }
+    class TresSur
+    {
+
+    }
+    class Sur
+    {
+
+    }
+    class noteperdue
+    {
+        private float note;
+        public void PerduNote()
+        {
+
+        }
+    }
+    class notegagne
+    {
+        private float note;
+        public void ajouteNote()
+        {
+
+        }
+    }
+    class noteFinal
+    {
+        public float note;
+        public float calculdunote()
+        {
+            return note;
+        }
+    }
+    static void Main(string[] args)
+    {
+    }
+}
